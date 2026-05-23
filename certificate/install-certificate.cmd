97502c499dbff07e5c06863923c546a7daad892a
Maciej Tomczak <icywire@gmail.com>
2026-05-23 23:10:35 +0200

    Update cert with new capabilities

--- certificate/install-certificate.cmd
+++ certificate/install-certificate.cmd
@@ -1,5 +1,5 @@
 certutil -f -p "falcon" -importpfx My "Project Falcon.pfx"
 
 certutil -f -addstore My "Project Falcon.cer"
-certutil -f -addstore Root "Project Falcon.cer"
-certutil -f -addstore TrustedPublisher "Project Falcon.cer"
+certutil -f -addstore -enterprise Root "Project Falcon.cer"
+certutil -f -addstore -enterprise TrustedPublisher "Project Falcon.cer"
